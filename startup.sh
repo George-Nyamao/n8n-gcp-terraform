d139bc85f45b2023a36b028cf2b71e67681d9d9d
George Nyamao <gmnyamao@hotmail.com>
2025-08-03 22:52:13 -0500

    change startup.sh

--- startup.sh
+++ startup.sh
@@ -16,7 +16,7 @@ docker run -d --name n8n \
   -p 5678:5678 \
   -v /root/n8n_data:/home/node/.n8n \
   -e N8N_BASIC_AUTH_ACTIVE=true \
-  -e N8N_BASIC_AUTH_USER=add \
+  -e N8N_BASIC_AUTH_USER=admin \
   -e N8N_BASIC_AUTH_PASSWORD=changeme \
   -e N8N_HOST=$DOMAIN \
   n8nio/n8n
